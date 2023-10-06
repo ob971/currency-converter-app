f216f557c7a68eef4cf17ef42ee65ca5b514b1a7
obsa <obsukebede2022@gmail.com>
2023-10-06 12:00:00 +0300

    flutter

--- lib/main.dart
+++ lib/main.dart
@@ -4,85 +4,56 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   @override
-  _MyAppState createState() => _MyAppState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
 }
 
-class _MyAppState extends State<MyApp> {
-  final myController = TextEditingController();
-
+class MyHomePage extends StatefulWidget {
   @override
-  void dispose() {
-    myController.dispose();
-    super.dispose();
-  }
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final myController = TextEditingController();
+  double result = 0.0;
 
   @override
   Widget build(BuildContext context) {
-    double result = 0;
-    TextStyle textStyle = TextStyle(
-        fontSize: 55,
-        fontWeight: FontWeight.bold,
-        color: const Color.fromARGB(255, 129, 115, 115));
-
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("currency conveter"),
-          elevation: 0,
-          backgroundColor: const Color.fromARGB(255, 129, 115, 115),
-        ),
-        backgroundColor: Color.fromARGB(255, 82, 66, 39),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                result.toString(),
-                style: textStyle,
-              ),
-              TextField(
-                controller: myController,
-                keyboardType: TextInputType.numberWithOptions(
-                    decimal: true, signed: true),
-                style: TextStyle(
-                  color: Colors.black,
-                ),
-                cursorColor: Colors.green,
-                cursorWidth: 2.0,
-                decoration: InputDecoration(
-                  contentPadding: EdgeInsets.all(12.0),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  hintText: 'Please enter the amount in INR',
-                  hintStyle: TextStyle(
-                    color: Colors.black,
-                  ),
-                  prefixIcon: Icon(Icons.monetization_on,
-                      color: Color.fromARGB(255, 92, 215, 112)),
-                  filled: true,
-                  fillColor: Colors.white,
-                ),
-              ),
-              TextButton(
-                onPressed: () {
-                  print(myController.text);
-                  setState(() {
-                    result = double.parse(myController.text);
-                  });
-                  FocusScope.of(context).unfocus(); // Dismiss the keyboard
-                },
-                child: Text('convert'),
-                style: TextButton.styleFrom(
-                  backgroundColor: Color.fromARGB(255, 76, 175, 84),
-                  foregroundColor: Colors.white,
-                  minimumSize: Size(200, 50),
-                ),
-              ),
-            ],
-          ),
+    return Scaffold(
+      backgroundColor:
+          Color.fromARGB(255, 126, 55, 83), // Set the background color here
+      appBar: AppBar(
+        title: Text('Currency Converter'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Result: $result Ethiopia Birr',
+              style: TextStyle(fontSize: 24),
+            ),
+            TextField(
+              controller: myController,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(hintText: 'Enter the amount in USD'),
+            ),
+            ElevatedButton(
+              child: Text('Convert'),
+              onPressed: () {
+                setState(() {
+                  result = double.parse(myController.text) *
+                      115; // Conversion rate to Ethiopia Birr
+                });
+                FocusScope.of(context).unfocus(); // Dismiss the keyboard
+              },
+            ),
+          ],
         ),
       ),
     );
